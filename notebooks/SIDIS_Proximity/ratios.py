0b7b678649c081b3091a379136c14c7bf46070a8
Scott <Juniper82>
2020-01-02 12:38:37 -0500

    SKD

--- notebooks/SIDIS_Proximity/ratios.py
+++ notebooks/SIDIS_Proximity/ratios.py
@@ -1,57 +1,28 @@
-#!/usr/bin/env python
-from __future__ import division
-import numpy as np
-import ratlib as rat
-from numpy import sqrt,log,exp
-import affinity as af
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 29 18:26:56 2019
 
+@author: scott
+"""
 
+from numpy import *
 
-
-
-
-if __name__=="__main__":
-
-
-    M= .94
-    M_h=.14
-    x_bj = .2
-    Q = 3
-    z_h = .3
-    P_t = 0.1
-
-    #N= 1000
-    xi = .3 #np.random.uniform(0.3,0.4,N)
-    zeta = .3 #np.random.uniform(0.3,0.4,N)
-    delta_k_t = .3 #np.random.uniform(0.3,0.4,N)
-    k_i_t = .2 #np.random.uniform(0,0.1,N)
-    M_ki = .1 #np.random.uniform(0,0.1,N)
-    M_kf = .1 #np.random.uniform(0,0.1,N)
-    #M= .938
-    #M_h=.135
-    #eta = rat.get_eta( M,M_h,x_bj,z_h,Q,q_t)
-
-    #xi = np.random.uniform(0.3,0.4,N)
-    #zeta = np.random.uniform(0.3,0.4,N)
-    #delta_k_t = np.random.uniform(0.3,0.4,N)
-    #k_i_t = np.random.uniform(0,0.1,N)
-    #M_ki = np.random.uniform(0,0.1,N)
-    #M_kf = np.random.uniform(0,0.1,N)
-   
+def ratios(x,z,Q2,pT,Mh,M,zeta,xi,ki,kf,kT,kiT,deltakT,phi):
     
-    q_t = -P_t/z_h
-    eta = .1
-
-
-    R1 = rat.get_R1( M,M_h,x_bj,z_h,eta,Q,q_t,xi,zeta,delta_k_t,k_i_t,M_ki,M_kf)
-    R2 = rat.get_R2( M,M_h,x_bj,z_h,eta,Q,q_t,xi,zeta,delta_k_t,k_i_t,M_ki,M_kf)
+    R0 = (maximum(maximum(ki**2/Q2,kf**2/Q2),kT**2/Q2))
     
-
-
-
+    R1 = 2*Q2*x*xi*z*(Mh**2+zeta**2*(deltakT**2+kf**2))*(sqrt((-4*M**2*x**2*(Mh**2+pT**2)+Q2**2*z**2) \
+         /(Q2**2*z**2))+1)/(zeta*(Q2**2*xi**2*z**2*(sqrt((-4*M**2*x**2*(Mh**2+pT**2)+Q2**2*z**2) \
+         /(Q2**2*z**2))+1)**2 - 4*Q2*kiT*pT*x*xi*z*(sqrt((-4*M**2*x**2*(Mh**2+pT**2)+Q2**2*z**2) \
+         /(Q2**2*z**2))+1)*cos(phi)+4*x**2*(Mh**2+pT**2)*(kf**2+kiT**2)))
     
-    #print(len(R1))
-    print(R1)
-    print(R2)
-
+    R2 = abs((Q2*z**2*(sqrt((-4.*M**2*x**2*(Mh**2+pT**2)+Q2**2*z**2)/(Q2**2*z**2))+1)**2-Q2*z*zeta \
+         *(sqrt((4.*M**2*x**2+Q2)/Q2)+1)*(sqrt((-4.*M**2*x**2*(Mh**2+pT**2)+Q2**2*z**2)/(Q2**2*z**2))+1) \
+         - pT**2*(sqrt((4.*M**2*x**2+Q2)/Q2)+1)**2 - zeta*(sqrt((4*M**2*x**2+Q2)/Q2)+1) \
+         *(deltakT**2*zeta *(sqrt((4*M**2*x**2+Q2)/Q2)+1)+2*deltakT*pT*(sqrt((4.*M**2*x**2+Q2)/Q2)+1)  \
+         +kf**2*(-z*(sqrt((-4*M**2*x**2*(Mh**2+pT**2)+Q2**2*z**2)/(Q2**2*z**2))+1) \
+         +zeta*(sqrt((4*M**2*x**2+Q2)/Q2)+1))))/(Q2*z*zeta*(sqrt((4.*M**2*x**2+Q2)/Q2)+1) \
+         *(sqrt((-4*M**2*x**2*(Mh**2+pT**2)+Q2**2*z**2)/(Q2**2*z**2))+1)))
     
+    return R0,R1,R2
